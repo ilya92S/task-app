bad2708575a5c4b3d93f4c60c2ce0f7abcc9aa9b
Ilya92S <saitovilya26@gmail.com>
2024-09-08 14:30:42 +0300

    add db&cache values to settings

--- settings.py
+++ settings.py
@@ -9,5 +9,8 @@ class Settings(BaseSettings):
     DB_PASS: str = "password"
     DB_USER: str = "admin"
 
+    CACHE_HOST: str = "localhost"
+    CACHE_PORT: int = 6378
+    CACHE_DB: int = 0
 
 settings = Settings()
